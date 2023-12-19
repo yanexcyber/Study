c588948e2a50c82557fc7e2d3a2999fd8a6612a7
Yanex <yanexcyber@proton.me>
2023-12-19 05:22:29 -0300

    .

--- assemblyStudy/Codigos/helloworld.asm
+++ assemblyStudy/Codigos/helloworld.asm
@@ -4,13 +4,20 @@
     ;e quantos bytes ele ocupa e se move pela memória
 
 ;O código de máquina em si é binário, então, o ASSEMBLER é o mais próximo que podemos chegar de escrever código de máquina;
+
 ;Procurar na internet pelo livro: Guia Mangá de Microprocessadores
-;aqui é onde eu inicio o programa. Quando criamos um programa ele executa em nível que é chamado de modo de usuário;
+
+
 ;Um processador possui uma arquitetura de proteção chamada de Rings, que são anéis de proteção. O Ring 0 é o mais privilegiado onde o kernel do S.O roda;
+
 ;No ring 0 é possível controlar completamente o hardware, e no ring 3 é onde os programas do usuário roda, e ele não tem acesso ao hardware;
+
 ;Por quando criamos um programa em ring 3, ele não tem acesso ao hardware. Ou seja não podendo acessar o ring 0
+
 ;A interrupção é a forma de chamar o processador para executar uma função. E a interrupção 80h é a interrupção que chama o kernel do S.O para executar uma função;
             
+
+;Aqui é onde eu inicio o programa. Quando criamos um programa ele executa em nível que é chamado de modo de usuário;
 
 section .text
 global _start
@@ -26,10 +33,6 @@ _start:
     mov eax, 0x01 ;movendo o valor 1 para o registrador eax para fechar o programa
     mov edx, 0x00 ;movendo o valor 0 para o registrador edx pois não temos nada para passar para o kernel do S.O
     int 0x80      ;chamando a interrupção 0x80 para executar a função de fechar o programa
-    
-
-
-
 
 ;Criando uma nova seção para o código de máquina, a seção de dados;
 
@@ -40,4 +43,3 @@ section .data
                     ;Se movermos uma string para um endereço de memória já utilizado, ele será sobrescrito, então, temos que saber o tamanho da string para não sobrescrevermos a memória.
 
 
-                    
